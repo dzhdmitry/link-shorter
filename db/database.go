63857d2787ae49696e2b5b4d26ab14429af27489
dmitry <d.dzhuleba@robo.cash>
2024-02-11 19:38:55 +1000

    actions #2

--- db/database.go
+++ db/database.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/lib/pq"
 	"os"
@@ -42,6 +43,12 @@ func Open(dsn string, maxOpenConns int, maxIdleConns int, maxIdleTime string) (*
 
 func ResolveTestDSNs() (string, string) {
 	defaultDsn := os.Getenv("TEST_DEFAULT_DSN")
+
+	fmt.Println("POSTGRES_HOST:")
+	fmt.Println(os.Getenv("POSTGRES_HOST"))
+
+	fmt.Println("TEST_DEFAULT_DSN:")
+	fmt.Println(os.Getenv("TEST_DEFAULT_DSN"))
 
 	if defaultDsn == "" {
 		defaultDsn = "postgres://go:pa55word@localhost:5432/postgres?sslmode=disable"
